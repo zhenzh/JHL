4a99e9aa36945b0d07bce4bd4d4c7a123475631f
zhenzh <79227800@qq.com>
2020-12-21 22:06:03 +0800

    更新支持 gomud 的 timer 框架

--- script/frame/timer.lua
+++ script/frame/timer.lua
@@ -2,6 +2,19 @@ timer = timer or {}
 timers = timers or {}
 timers.group = timers.group or {}
 
+function call_timer_actions(name)
+    if timer.is_exist(name) == false then
+        return nil
+    end
+    local rc = loadstring(timers[name].send)()
+    if timer.is_exist(name) == true then
+        if timers[name].options.OneShot == true then
+            timer.delete(name)
+        end
+    end
+    return rc
+end
+
 function timer.add(name, seconds, send, group, options)
     if name == nil or name == "" then
         name = "timer_"..unique_id()
@@ -15,25 +28,23 @@ function timer.add(name, seconds, send, group, options)
     options = options or {}
 
     timers[name] = { send = send, group = group, seconds = seconds, options = options }
-    if not options.OneShot then
-        timers[name].id = tempTimer(seconds, send, true)
-    else
-        send = send.." timer.delete('"..name.."')"
-        timers[name].id = tempTimer(seconds, send)
-    end
     if group ~= nil then
-        timers.group[group][name] = timers[name].id
+        timers.group[group][name] = true
     end
-    if options.Enable == false then
-        timer.disable(name)
+    if options.OneShot == true then
+        if options.Enable ~= false then
+            AddTimer(name, "show('test1')", seconds*1000, 1)
+        end
     else
-        timer.enable(name)
+        if options.Enable ~= false then
+            AddTimer(name, "show('test2')", seconds*1000, 0)
+        end
     end
     return name
 end
 
 function timer.delete(name)
-    local rc = killTimer((timers[name] or {}).id or "")
+    local rc = DelTimer(name)
     if timers[name] ~= nil and timers[name].group ~= nil then
         timers.group[timers[name].group][name] = nil
         if table.is_empty(timers.group[timers[name].group]) then
@@ -45,15 +56,30 @@ function timer.delete(name)
 end
 
 function timer.enable(name)
-    return enableTimer((timers[name] or {}).id or name)
+    if timer.is_exist(name) == false then
+        return false
+    end
+    if timer.is_enable(name) == true then
+        return true
+    end
+    timers[name].options.Enable = true
+    if timers[name].options.OneShot == true then
+        return AddTimer(name, timers[name].send, timers[name].seconds*1000, 1)
+    else
+        return AddTimer(name, timers[name].send, timers[name].seconds*1000, 0)
+    end
 end
 
 function timer.disable(name)
-    return disableTimer((timers[name] or {}).id or name)
+    if timer.is_exist(name) == false then
+        return false
+    end
+    DelTimer(name)
+    timers[name].options.Enable = false
 end
 
 function timer.remain(name)
-    return remainingTime(timers[name].id)
+    return "unknown"
 end
 
 function timer.is_exist(name)
@@ -68,10 +94,10 @@ function timer.is_enable(name)
     if timer.is_exist(name) == false then
         return false
     end
-    if isActive(timers[name].id, "timer") == 0 then
-        return false
-    else
+    if timers[name].options.Enable == true then
         return true
+    else
+        return false
     end
 end
 
